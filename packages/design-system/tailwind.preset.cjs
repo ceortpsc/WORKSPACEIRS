90cb0215ef66cce906cd304e064dacddcfcd4334
ceortpsc <ceo@rosstaxsoftware.com>
2026-07-14 05:20:59 -0500

    style(design): add eggshell tan Tailwind palette

--- packages/design-system/tailwind.preset.cjs
+++ packages/design-system/tailwind.preset.cjs
@@ -7,7 +7,9 @@ module.exports = {
         ross: {
           navy: { 950:'#03101F',900:'#071A33',800:'#0B294D',700:'#123D6A' },
           gold: { 600:'#A98214',500:'#C9A227',400:'#E0BE4F' },
-          mist:'#F4F7FA', slate:'#52606D', success:'#137A52', warning:'#A35B00', danger:'#B42318', info:'#175CD3'
+          eggshell: { 50:'#FFFAF1',100:'#FFF6E8',200:'#F8EDDB',300:'#F1E2CA',400:'#E5D2B5',500:'#D4BC98' },
+          base:'#F8EDDB', surface:'#FFFAF1', elevated:'#FFFDF8',
+          slate:'#52606D', success:'#137A52', warning:'#A35B00', danger:'#B42318', info:'#175CD3'
         }
       },
       fontFamily: {
@@ -15,9 +17,9 @@ module.exports = {
         mono:['JetBrains Mono','ui-monospace','monospace']
       },
       boxShadow: {
-        'ross-sm':'0 1px 2px rgb(16 24 40 / .08)',
-        'ross-md':'0 8px 24px rgb(7 26 51 / .12)',
-        'ross-lg':'0 20px 45px rgb(7 26 51 / .18)'
+        'ross-sm':'0 1px 2px rgb(74 52 28 / .09)',
+        'ross-md':'0 8px 24px rgb(74 52 28 / .13)',
+        'ross-lg':'0 20px 45px rgb(74 52 28 / .18)'
       }
     }
   },
